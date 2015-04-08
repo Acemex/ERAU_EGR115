9760dc2dea341b11be48b89574e087a627474fce
Adam Metruck <metrucka@my.erau.edu>
2015-04-08 18:41:03 -0400

    Added functional input code for the keypad. Accepts 4 inputs, then checks if they're valid against a fixed value.
    Signed-off-by: Adam Metruck <metrucka@my.erau.edu>

--- Project_r1.ino
+++ Project_r1.ino
@@ -3,11 +3,13 @@
 #include <Servo.h>
 #include <Keypad.h>
 
-const char led_red = A0; // Set up constant values for pin assignments
-const char led_yellow = A1;
+const char led_red = A5; // Set up constant values for pin assignments
+const char led_yellow = A4;
 const char servo = 9;
 const char sdpin = 4;
 Servo servo1;
+File myFile;
+//char buf[10];
 
 
 
@@ -24,40 +26,63 @@ Servo servo1;
 		{ '*', '0', '#' }
 	};
 
-	byte rowPins[rows] = { 0, 1, 2, 3 }; //connect row pins on board
-	byte colPins[cols] = { 5, 6, 7 }; //connect column pins on board
+	//byte rowPins[rows] = { 0, 1, 2, 3 }; //connect row pins on board
+	//byte colPins[cols] = { 5, 6, 7 }; //connect column pins on board
+	byte rowPins[rows] = { 29, 31, 33, 35 }; //MEGA pinout for debug; frees up pins 0 and 1
+	byte colPins[cols] = { 39, 41, 43 }; //MEGA pinout for debug; frees up pins 0 and 1
 	
 	Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, rows, cols);
 
 	
 void setup()
 	{
-	servo1.attach(servo); //Attach the the servo to pin 9.
-	servo1.write(90); //Move the servo to neutral for testing
+	//servo1.attach(servo); //Attach the the servo to pin 9.
+	//servo1.write(90); //Move the servo to neutral for testing
 	
 	SD.begin(sdpin);
+
+	Serial.begin(9600);
+	Serial.println("Hello, World! Lock program beginning");
 
 }
 
 void loop()
 {
-	char instr[4];
+	String instr = "0000";
 	
 	int incount = 0;
-	while (incount < 3)
+
+	while (incount < 4)
 	{
 		char next = keypad.waitForKey();
 		
-		instr[incount] = next;
+		Serial.println(next);
 
+		instr.setCharAt(incount, next);
 
-		for (int flash = 0; flash < next; flash++ )
-		{
-			digitalWrite(led_red, 1);
-			delay(100);
-			digitalWrite(led_red, 0);
-
-		}
+		Serial.println(instr);
+		incount++;
+		
 	}
+
+	
+
+	if (instr == "1234")
+	{
+		digitalWrite(led_yellow, HIGH);
+		Serial.println("Pattern successful");
+		delay(5000);
+		digitalWrite(led_yellow, LOW);
+	}
+	else
+	{
+		digitalWrite(led_red, HIGH);
+		Serial.println("Pattern failed!");
+		delay(5000);
+		digitalWrite(led_red, LOW);
+		
+	}
+
+
 
 }
